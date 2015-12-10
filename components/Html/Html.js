7cb503b7babe7b17101e1130880f4f6034c2fcc2
patmood <patrick.n.moody@gmail.com>
2015-12-09 20:01:15 -0800

    base url

--- components/Html/Html.js
+++ components/Html/Html.js
@@ -15,6 +15,7 @@ function Html({ title, description, body, debug }) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <title>{title || config.title}</title>
+        <base href="/svg-jsx-online/" />
         <meta name="description" content={description || config.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
